7054365c07445ad86f97736fbba65c7b11574463
Stan004 <flywwww004@gmail.com>
2019-02-26 17:26:11 +0800

    Setting done

---
+++ famibook/settings/prod.py
@@ -0,0 +1,21 @@
+from .common import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+ALLOWED_HOSTS = []
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'qpf6&77oy#48$y$-ko+v_6-!&ip&#a-k+1a@g9wejq0u72l!ct'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- famibook/wsgi.py
+++ famibook/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'famibook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'famibook.settings.dev')
 
 application = get_wsgi_application()
